55636d3e0b27ce06497c399103a44e3336f3673a
Mitch <me@miitch.io>
2020-08-04 19:38:20 +0800

    Fix schema ID

--- schema/1-add_users_table.sql
+++ schema/1-add_users_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
-    id integer PRIMARY KEY,
-    username varchar(24) NOT NULL,
-    email varchar(255) NOT NULL,
-    password bytea NOT NULL,
-    secret varchar(19) NOT NULL
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(24) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password BYTEA NOT NULL,
+    secret VARCHAR(19) NOT NULL
 );
--- schema/2-add_notebooks_table.sql
+++ schema/2-add_notebooks_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS notebooks (
-    id integer PRIMARY KEY,
-    name varchar(24) NOT NULL,
-    color varchar(6) NOT NULL,
-    user_id integer NOT NULL,
-    created_at integer NOT NULL,
-    updated_at integer NOT NULL
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(24) NOT NULL,
+    color VARCHAR(6) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at INTEGER NOT NULL,
+    updated_at INTEGER NOT NULL
 );
