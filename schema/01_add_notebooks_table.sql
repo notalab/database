69f9d4090ac6a6f0d8d1738415e9eb68e5033c09
mtchdev <fsmitchm@gmail.com>
2020-08-04 13:57:10 +0800

    Dockerfile

--- schema/01_add_notebooks_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `notebooks` (
-    `id` integer PRIMARY KEY,
-    `name` varchar(24) NOT NULL,
-    `color` varchar(6) NOT NULL,
-    `user` integer NOT NULL,
-    `created_at` integer NOT NULL,
-    `updated_at` integer NOT NULL
-);
--- schema/1-add_users_table.sql
+++ schema/1-add_users_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS users (
     id integer PRIMARY KEY,
     username varchar(24) NOT NULL,
     email varchar(255) NOT NULL,
-    `password` binary NOT NULL,
-    `secret` varchar(19) NOT NULL
+    password bytea NOT NULL,
+    secret varchar(19) NOT NULL
 );
